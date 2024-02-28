f07dc113cc4668bd77f4728867fce7218cbdad18
anubis <anubis@example.com>
2024-02-28 15:04:05 +0100

    add doControllerRequest

--- index.js
+++ index.js
@@ -23,11 +23,57 @@ SFS2X.HypergamingControllerRequest.FREEROUNDS = 2;
 SFS2X.HypergamingControllerRequest.FREEROUND = 3;
 SFS2X.HypergamingControllerRequest.LOYALTY_RESOLVE = 4;
 SFS2X.HypergamingControllerRequest.LOYALTY_CLAIM = 5;
-SFS2X.HypergamingControllerRequest.GAME_LIST = 6;
-SFS2X.HypergamingControllerRequest.GAME_TAGS = 7;
-SFS2X.HypergamingControllerRequest.CREATE_SESSION = 8;
-SFS2X.HypergamingControllerRequest.JACKPOTS_CONTRIB = 9;
-SFS2X.HypergamingControllerRequest.JACKPOT_WIN = 10;
-SFS2X.HypergamingControllerRequest.USER_LOGIN = 11;
+SFS2X.HypergamingControllerRequest.LOYALTY_UPDATE = 6;
+SFS2X.HypergamingControllerRequest.GAME_LIST = 7;
+SFS2X.HypergamingControllerRequest.GAME_TAGS = 8;
+SFS2X.HypergamingControllerRequest.CREATE_SESSION = 9;
+SFS2X.HypergamingControllerRequest.JACKPOTS_CONTRIB = 10;
+SFS2X.HypergamingControllerRequest.JACKPOT_WIN = 11;
+SFS2X.HypergamingControllerRequest.USER_LOGIN = 12;
+
+
+
+SFS2X.SmartFox.prototype.doControllerRequest = async function(reqId, payload = {}) {
+    let self = this;
+    let request = new SFS2X.HypergamingControllerRequest(reqId, SFS2X.SFSObject.fromObject(payload));
+    
+    this.send(request);
+    return new Promise(function (resolve, reject) {
+
+        const evtHandler = ({content, requestId}) => {
+            let obj = SFS2X.SFSObject.toObject(content);
+            if(reqId == requestId){
+                self.removeEventListener(SFS2X.SFSEvent.HYPERGAMING_CONTROLLER_RESPONSE, evtHandler);
+                resolve(obj);
+            }
+        }
+        
+        self.addEventListener(SFS2X.SFSEvent.HYPERGAMING_CONTROLLER_RESPONSE, evtHandler, this);
+    })
+}
+SFS2X.SmartFox.prototype._connect = SFS2X.SmartFox.prototype.connect;
+SFS2X.SmartFox.prototype.connect = async function(reqId, payload = {}) {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+
+        const onConnection = (event) => {
+            try {
+                if (event.success) {
+                    resolve()
+                }
+                else {
+                    reject(event.errorMessage)
+                    console.warn('Connection failed: ' + (event.errorMessage ? event.errorMessage + '; (' + event.errorCode + ')' : 'Is the server running at all?'));
+                }
+            } finally {
+                self.removeEventListener(SFS2X.SFSEvent.CONNECTION, onConnection);
+            }
+            
+        }
+        
+        self.addEventListener(SFS2X.SFSEvent.CONNECTION, onConnection, this);
+        self._connect();
+    });
+}
 
 module.exports = SFS2X;
